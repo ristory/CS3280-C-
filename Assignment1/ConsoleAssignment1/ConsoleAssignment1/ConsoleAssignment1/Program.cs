45beb285685cff2b078cfcb6f6d3488b9aeb50de
Hoang Cao <lastristory12@yahoo.com>
2017-05-07 03:11:58 -0600

    Add files via upload

---
+++ Assignment1/ConsoleAssignment1/ConsoleAssignment1/ConsoleAssignment1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+
+            Console.WriteLine("Please enter the first number: "+ a );
+            Console.WriteLine("Please enter the first number: " + b);
+            Console.WriteLine("a + b = " + (a + b));
+            Console.WriteLine("a - b = " + (a - b));
+            Console.WriteLine("a * b = " + (a * b));
+            Console.WriteLine("a / b = " + (a / b));
+            Console.WriteLine("a % b = " + (a % b));
+            if (a > b)
+            {
+                Console.WriteLine(a +" is greater than " + b);
+            }
+            else if (a < b)
+            {
+                Console.WriteLine(a + " is less than " + b);
+            }
+            else if (a == b)
+            {
+                Console.WriteLine(a + " is equal " + b);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Assignment1/WindowsAssignment1/WindowsAssignment1/CS3280/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS3280
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Input Product Information","Product", MessageBoxButtons.OKCancel, MessageBoxIcon.Question)==DialogResult.OK)
+            {
+                MessageBox.Show(textBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("Have a great day");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Input Company Information", "Information", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                MessageBox.Show(textBox2.Text);
+            }
+            else
+            {
+                MessageBox.Show("Have a great day");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Input Policy Information", "Policy", MessageBoxButtons.OK, MessageBoxIcon.Warning) == DialogResult.OK)
+            {
+                MessageBox.Show(textBox3.Text);
+            }
+            else
+            {
+                MessageBox.Show("Have a great day");
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
